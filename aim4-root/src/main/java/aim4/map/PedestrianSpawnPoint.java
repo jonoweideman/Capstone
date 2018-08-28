9879778d5fe8e2c475029fb5c836114fe09c886e
Robbie Perrott <PRRROB006@myuct.ac.za>
2018-08-28 10:02:41 +0200

    Started making the pedestrians

---
+++ aim4-root/src/main/java/aim4/map/PedestrianSpawnPoint.java
@@ -0,0 +1,25 @@
+package aim4.map;
+
+import java.awt.geom.Point2D;
+
+public class PedestrianSpawnPoint {
+	
+	// Location of the spawning point
+	private Point2D location;
+	// Destination of this spawning point
+	private PedestrianSpawnPoint destinationSpawnPoint;
+	
+	public PedestrianSpawnPoint(Point2D location, PedestrianSpawnPoint destinationSpawnPoint) {
+		this.location = location;
+		this.destinationSpawnPoint = destinationSpawnPoint;
+	}
+	
+	public Point2D getLocation() {
+		return location;
+	}
+	
+	public Point2D getDestination() {
+		return location;
+	}
+	
+}
---
+++ aim4-root/src/main/java/aim4/pedestrian/Pedestrian.java
@@ -0,0 +1,33 @@
+package aim4.pedestrian;
+import java.awt.geom.Point2D;
+
+import aim4.im.Intersection;
+import aim4.im.RoadBasedIntersection;
+
+public class Pedestrian {
+	
+	Point2D p;
+	
+	// Private fields
+	//private Intersection intersection;
+	//private int start;
+	//private int end;
+	
+	// Constructor
+	//public Pedestrian(Intersection intersection, int start, int end) {
+		//this.intersection = intersection;
+		//this.start = start;
+		//this.end = end;
+	//}
+	
+	//public void cross() {
+		
+	//}
+	
+	//public void spawn() {
+		//push button if needed
+		
+		// put in constructor?
+	//}
+	
+}
